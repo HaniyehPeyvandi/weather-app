a3b48dcd2e6f11f016ed32abab1f570ad6fb658d
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-12-15 00:14:52 +0330

    add elements to WeatherDetails component

--- src/components/WeatherDetails/WeatherDetails.js
+++ src/components/WeatherDetails/WeatherDetails.js
@@ -1,7 +1,54 @@
+import styles from "./WeatherDetails.module.css";
+
 const WeatherDetails = () => {
-  return ( 
-    <div>WeatherDetails Component</div>
-   );
-}
- 
+  return (
+    <div className={styles.weatherContainer}>
+      <div className={styles.weatherIcon}><i className="wi wi-day-cloudy"></i></div>
+      <div className={styles.weatherDesc}>
+        <div className={styles.degree}>
+          <p>32.05&deg;</p>
+        </div>
+        <div className={styles.weatherInfo}>
+          <p className={styles.weatherCondition}>HAZE</p>
+          <p>Tehran, IRAN</p>
+        </div>
+        <div className={styles.time}>
+          <p>27/9/2021,</p>
+          <p>2:21:20 pm</p>
+        </div>
+      </div>
+      <div className={styles.weatherDetails}>
+        <div className={styles.weatherItem}>
+          <div><i className="wi wi-sunrise"></i></div>
+          <div>
+            <p>6:12 AM</p>
+            <p>Sunrise</p>
+          </div>
+        </div>
+        <div className={styles.weatherItem}>
+          <div><i className="wi wi-sunset"></i></div>
+          <div>
+            <p>18:12 PM</p>
+            <p>Sunset</p>
+          </div>
+        </div>
+        <div className={styles.weatherItem}>
+          <div><i className="wi wi-humidity"></i></div>
+          <div>
+            <p>66</p>
+            <p>Humidity</p>
+          </div>
+        </div>
+        <div className={styles.weatherItem}>
+          <div><i className="wi wi-strong-wind"></i></div>
+          <div>
+            <p>1.54</p>
+            <p>Speed</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default WeatherDetails;

c16c7d9c70bcae01c36368c8fb5b9a1cf34ca8d6
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-12-14 12:54:16 +0330

    get the search value

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,12 +1,21 @@
-import styles from './Search.module.css';
+import { useState } from "react";
+import styles from "./Search.module.css";
 
 const Search = () => {
-  return ( 
+  const [cityName, setCityName] = useState("tehran");
+
+  return (
     <div className={styles.searchContainer}>
-      <input type="text" placeholder="Enter city name..." className={styles.searchInput}/>
+      <input
+        type="text"
+        placeholder="Enter city name..."
+        className={styles.searchInput}
+        value={cityName}
+        onChange={(e) => setCityName(e.target.value)}
+      />
       <button className={styles.searchBtn}>Search</button>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Search;

1eec2ddeff0ead38f8cced2f3b2a596ee2337d20
HaniyehPeyvandi <hpeyvandi0@gmail.com>
2022-12-18 11:35:33 +0330

    add react-toastify to the project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import WeatherApp from './components/WeatherApp/WeatherApp';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div className="App">
       <WeatherApp/>
+      <ToastContainer />
     </div>
   );
 }
